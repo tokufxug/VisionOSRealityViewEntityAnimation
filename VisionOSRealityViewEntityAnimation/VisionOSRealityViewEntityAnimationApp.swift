bf65f24bc501da54ad531fbc2ed11225ffdd19aa
tokufxug <tokufxug@gmail.com>
2023-12-24 23:18:12 +0900

    Initial Commit

---
+++ VisionOSRealityViewEntityAnimation/VisionOSRealityViewEntityAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSRealityViewEntityAnimationApp.swift
+//  VisionOSRealityViewEntityAnimation
+//
+//  Created by Sadao Tokuyama on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSRealityViewEntityAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
